accff6957b538387d1d3b218851180a9ba659497
Annie <annie28750@gmail.com>
2023-04-25 11:56:08 +0800

    fix: allow all hosts

--- backend/settings.py
+++ backend/settings.py
@@ -127,6 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+ALLOWED_HOSTS = ["*"]
+
 CORS_ORIGIN_WHITELIST = [
     "https://marionia.herokuapp.com"
 ]
